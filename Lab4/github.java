d856ea12037a3bd2c7ef6ff57f0b909ed4df517a
backker007 <blacks.anp18@gmail.com>
2024-02-04 16:24:31 +0700

    1

--- Lab4/github.java
+++
@@ -1,9 +0,0 @@
-package Lab4;
-
-public class github {
-
-    public static void main(String[] args) {
-        System.out.println("hello githb");
-        System.out.println("hello githb");
-    }
-}
